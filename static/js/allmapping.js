2fd09b0680421b5e11c95b8c0338522c8541b033
Dipesh R Manandhar <dipesh267@hotmail.com>
2018-09-17 09:38:08 -0700

    minzoom set for map

--- static/js/allmapping.js
+++ static/js/allmapping.js
@@ -7,6 +7,7 @@ data = coord_data;
 var light = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
+    minZoom: 10,
     id: "mapbox.streets",
     accessToken: API_KEY
     });
