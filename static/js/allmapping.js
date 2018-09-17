c248df9b3b812b370c017ac735627c8590e03a25
Dipesh R Manandhar <dipesh267@hotmail.com>
2018-09-17 12:36:00 -0700

    cleaning up

--- static/js/allmapping.js
+++ static/js/allmapping.js
@@ -1,23 +1,14 @@
 data = coord_data;
 
-// Once we get a response, send the data.features object to the createFeatures function
-//console.log(data);
-
 // console.log(data.features);
 var light = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    minZoom: 10,
+    minZoom: 11,
     id: "mapbox.streets",
     accessToken: API_KEY
     });
 
-// var dark = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   maxZoom: 18,
-//   id: "mapbox.dark",
-//   accessToken: API_KEY
-// });
 var featureList = data;
     
 //var listingMarkers = [];
